3b73063abecf8834760e45c12d5e2ab859c3502e
Jonas <jonasbenti@hotmail.com>
2022-07-10 23:35:02 -0300

    Correcao imagens e rodape

--- public/index.php
+++ public/index.php
@@ -2,8 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$path = isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : '/home';
+$path = isset($_SERVER['HEROKU_APP_DIR']) ? $_SERVER['REDIRECT_URL'] : $_SERVER['PATH_INFO'];
+PHP_EOL.PHP_EOL;
+var_dump($path);
+$path = $path ?: '/home';
 var_dump($_SERVER);
+PHP_EOL.PHP_EOL;
+var_dump($path);
 
 $routes = require __DIR__ . '/../Config/routes.php';
 
